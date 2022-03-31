9e3e0d7c46da13aad23e9f6dd92aa729aff2a07d
Semih Gecer <smhgcr@outlook.com>
2022-03-31 23:44:27 +0300

    maven ile tarayıcıdan parametrik test kosumu eklendi

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,27 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class BaseTest {
 
     protected WebDriver driver;
-
+    static String browser = System.getProperty("browser");
 
     @BeforeAll
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+    public static void setUp() {
+        if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+        } else if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+        }
+        // bonigarcia kutuphanesi kullanıldıgı icin buna gerek kalmadi
+        //System.setProperty("webdriver.chrome.driver","drivers/chromedriver")
         System.out.println("Test initiated");
 
     }
 
     @BeforeEach
-    public void beforeMethod(){
-        driver = new ChromeDriver();
+    public void beforeMethod() {
+       driver = getDriver(browser);
     }
 
     @AfterEach
-    public void afterMethod(){
+    public void afterMethod() {
         driver.quit();
     }
 
@@ -31,6 +39,14 @@ public class BaseTest {
         System.out.println("Test finished");
     }*/
 
-
+    private WebDriver getDriver(String browser) {
+        WebDriver driver = null;
+        if (browser.equals("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals("chrome")) {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
 
 }
